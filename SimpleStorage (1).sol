86fcaf41f9774ac195b4e4874a3331c4af4d1f35
murcialex99-fintech <murcialex99@gmail.com>
2026-01-17 22:23:45 +0100

    Update SimpleStorage (1).sol

--- SimpleStorage (1).sol
+++ SimpleStorage (1).sol
@@ -33,17 +33,18 @@ contract SimpleStorage {
 // ABOUT THE CONTRACT
 // This contract has 2 transactional functions (addPerson, store) and 4 call functions (retrieve, myfavoriteNumber, listOfPeople, nameToFavoriteNumber).
 // This contract allows the deployer to add persons with a number assigned to each. 
-// You can search for a number and a person when calling listOfPeople and nameToFavoriteNumber.
+// You can search for a number and a person when calling listOfPeople and nameToFavoriteNumber respectively.
 // This is possible mainly thanks to the mapping and array variables.
 // The contract also allows to store a specific number. The latest number shows up in myFavoriteNumber and retrieve when you call those functions.
 
 // GENERAL VIEW
-// This is my first ever smart contract in any language. It was written in Remix IDE and deployed in two different testnets (Sepolia mainnet and Sepolia zkSync).
+// This is my first ever smart contract. It was written in Remix IDE and deployed in two different testnets (Sepolia mainnet and Sepolia zkSync).
 // Before that, I created a MetaMask wallet, got Sepolia ETH from a faucet and bridged half of it into the zkSync network.
-// Everything was easy and very familiar to me as I have some XP in the crypto space. However, the coding part was quite complex and almost new to me.
-// The most exciting part to me was deploying the contract on chain and triggering the different functions and variables.
+// Everything was easy and very familiar to me as I have some XP in the crypto space. However, the coding part was quite complex.
+// The most exciting part to me was deploying the contract on chain and triggering the different functions and variables into txs & calls.
 
 // 0x22d90b4f061fCcFa4043323544F1F9ec79Cf1454 (Ethereum metamask address)
 // 0x65C7677C91A07e6aD21dC3e732A5FB7e72ef80C8 (sepolia zksync block explorer - contract address)
 // 0x22d90b4f061fCcFa4043323544F1F9ec79Cf1454 (sepolia ethereum block explorer - contract address)
+
 
