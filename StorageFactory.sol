31d733c9d569651d6d3b9f44a7cf1db8698aa33b
murcialex99-fintech <murcialex99@gmail.com>
2026-01-18 10:11:18 +0100

    Update StorageFactory.sol

--- StorageFactory.sol
+++ StorageFactory.sol
@@ -20,3 +20,15 @@ contract StorageFactory {
         return listOfSimpleStorageContracts[_simpleStorageIndex].retrieve();
     }
 }
+
+// ABOUT THE CONTRACT
+// This contract imports the SimpleStorage contract and allows anyone to deploy SimpleStorage contracts.
+// All contracts created are indexed into a list. Calling listOfSimpleStorageContracts function shows the address for each contract.
+// The contract also allows to assign a number to each indexed contract with the function sfStore.
+// Then the sfGet function returns the assigned number when you call it by introducing a contract's indexed number.
+// Imported contracts have to be in the same folder of the importer contract, and can be specified with "{} from ./".
+
+// GENERAL VIEW
+// The contracts interact "seamlessly" (as we say in the course) with each other and with wallet addresses.
+// The ABI from Remix IDE is fast and simple, and it also has an AI chatbot.
+// The contracts are easy to structure: License + Version > Imports, (if any) > Declaration (State variables > Functions).
